cbe7924a995720b2a168077059c2fed5af1dbc87
Ronaldo Regis <ronaldojprf@gmail.com>
2017-11-20 14:46:58 +1100

    Alert class: removed unnecessary null check and test.

--- src/main/java/LoadMonitoringSystem/model/Alert.java
+++ src/main/java/LoadMonitoringSystem/model/Alert.java
@@ -10,14 +10,9 @@ public class Alert {
     private final int min;
     private final int max;
 
-    private final static String ILLEGAL_CONTAINER_CODE_ARGUMENT = "Container code must be not null.";
-
     public Alert(String containerCode, int containerTemperature, int max, int min){
-        if(containerCode == null){
-            throw new IllegalArgumentException(ILLEGAL_CONTAINER_CODE_ARGUMENT);
-        }
-        this.containerCode = containerCode;
 
+        this.containerCode = containerCode;
         this.containerTemperature = containerTemperature;
         this.max = max;
         this.min = min;
